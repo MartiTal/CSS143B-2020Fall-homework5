29001e8f56beebb2b32e27c8df2dd17ad2c9a244
TalonM <talonm@uw.edu>
2020-11-14 22:08:27 -0800

    Problem 2 complete, test is passing

--- src/main/java/Problem2/LCA.java
+++ src/main/java/Problem2/LCA.java
@@ -6,6 +6,25 @@ public class LCA {
     public static TreeNode<Integer> lowestCommonAncestor(TreeNode<Integer> root, TreeNode<Integer> p, TreeNode<Integer> q) {
         // homework
         // hint: pay attention to the assumptions of this problem.
-        return null;    // place holder
+
+        if (root.val == p.val || root.val == q.val)
+            return root;
+
+        TreeNode<Integer> left = null;
+        if (root.left != null)
+            left = lowestCommonAncestor(root.left, p, q);
+
+        TreeNode<Integer> right = null;
+        if (root.right != null)
+            right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null)
+            return root;
+        else if (left == null && right == null)
+            return null;
+        else
+            return (right == null) ? left : right;
+        //This is my first time attempting to use this ? operator, I remember seeing it somewhere a while back.
+        //Is it worth using here or unnecessary?
     }
 }
