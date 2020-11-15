f2ef7414880133763e924b094fc921dab801bd5a
TalonM <talonm@uw.edu>
2020-11-14 22:41:08 -0800

    Problem 3.1 test passing

--- src/main/java/Problem3/InsertInBST.java
+++ src/main/java/Problem3/InsertInBST.java
@@ -5,5 +5,19 @@ import Problem1.TreeNode;
 public class InsertInBST {
     public static void insert(TreeNode<Integer> root, int valToInsert) {
         // homework
+        if (valToInsert <= root.val) {
+            if (root.left == null) {
+                root.left = new TreeNode<>(valToInsert);
+                return;
+            }
+            insert(root.left, valToInsert);
+        } else {
+            if (root.right == null) {
+                root.right = new TreeNode<>(valToInsert);
+                return;
+            }
+            insert(root.right, valToInsert);
+        }
+
     }
 }
